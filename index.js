9f26303355ff97324ab9b6c9f4e66f7110a1c102
iSweazZ <wissem26500@gmail.com>
2025-04-21 03:55:10 +0200

    Add encrypt support for es3 files

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ app.post('/decrypt', upload.single('file'), async (req, res) => {
     res.json(decryptedData.toString('utf-8'));
 });
 
+// Route POST /encrypt
+app.post('/encrypt', async (req, res) => {
+    const { data, password } = req.body;
+
+    if (!data || !password) {
+        return res.status(400).json({ error: 'Data and password are required.' });
+    }
+
+    try {
+        const encryptedData = await crypt.encryptData(Buffer.from(data, 'utf-8'), password);
+        res.json({ encryptedData: encryptedData.toString('utf-8') });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to encrypt data.' });
+    }
+});
+
 // Démarrage du serveur
 const PORT = 3000;
 app.listen(PORT, () => {
