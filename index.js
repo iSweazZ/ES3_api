4e0fb76b5c605fd267b233f0863197d0f99fde28
iSweazZ <wissem26500@gmail.com>
2025-04-27 07:12:04 +0200

    refacto

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const multer = require('multer');
+const cors = require('cors'); // Importer le module cors
 const crypt = require('./utils/crypt'); // Assurez-vous que le chemin est correct
+const fs = require('fs'); // Importer le module fs pour écrire des fichiers
+const http = require('http');
+const path = require('path');
 
 const app = express();
 const upload = multer();
 
+// Activer CORS pour toutes les routes
+app.use(cors());
+
 // Middleware pour parser les JSON
 app.use(express.json());
+app.use('/static/assets/images', express.static(path.join(__dirname, 'assets/images')));
 
 // Route POST /decrypt
 app.post('/decrypt', upload.single('file'), async (req, res) => {
@@ -27,22 +35,67 @@ app.post('/decrypt', upload.single('file'), async (req, res) => {
 
 // Route POST /encrypt
 app.post('/encrypt', async (req, res) => {
+    //console.log(req.body.password)
     const { data, password } = req.body;
 
     if (!data || !password) {
         return res.status(400).json({ error: 'Data and password are required.' });
     }
-
     try {
-        const encryptedData = await crypt.encryptData(Buffer.from(data, 'utf-8'), password);
+        const encryptedData = await crypt.encryptData(data, password);
+        const encryptedPath = './StoreFile4.es3';
+        fs.writeFileSync(encryptedPath, encryptedData);
         res.json({ encryptedData: encryptedData.toString('utf-8') });
     } catch (error) {
-        res.status(500).json({ error: 'Failed to encrypt data.' });
+        console.log(error)
+        res.status(500).json({ error: error.message });
     }
 });
 
-// Démarrage du serveur
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    switch (error.code) {
+        case 'EACCES':
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+const bootstrap = () => {
+    const port = normalizePort(process.env.PORT || '3000')
+    app.set('port', port)
+
+    const server = http.createServer(app)
+    server.on('error', errorHandler);
+    server.on('listening', () => {
+        const address = server.address();
+        const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+        console.log('Listening on ' + bind);
+    })
+
+    server.setTimeout(1000 * 60 * 5)
+    server.listen(port)
+}
+
+bootstrap();
--- utils/crypt.js
+++ utils/crypt.js
@@ -1,3 +1,10 @@
+/*
+*
+* sqlite
+* alwaysdata
+*
+* */
+
 const crypto = require('crypto');
 const ALGORITHM = 'aes-128-cbc';
 const PBKDF2_ITERATIONS = 100;
@@ -9,8 +16,7 @@ module.exports = (() =>{
         const key = crypto.pbkdf2Sync(password, iv, PBKDF2_ITERATIONS, 16, 'sha1'); // Dérive la clé
         const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
       
-        const decryptedData = Buffer.concat([decipher.update(encryptedData), decipher.final()]);
-        return decryptedData;
+        return Buffer.concat([decipher.update(encryptedData), decipher.final()]);
       };
 
       const encryptData = async (data, password) => {
@@ -18,11 +24,11 @@ module.exports = (() =>{
         const key = crypto.pbkdf2Sync(password, iv, PBKDF2_ITERATIONS, 16, 'sha1'); // Dérive la clé
         const cipher = crypto.createCipheriv(ALGORITHM, key, iv);
       
-        const encryptedData = Buffer.concat([iv, cipher.update(data), cipher.final()]);
-        return encryptedData;
+        return Buffer.concat([iv, cipher.update(data), cipher.final()]);
       }
 
       return {
-        decryptData
+        decryptData,
+        encryptData,
       };
 })()
